399c1d687c9d26813df8a89b0396d528540fc640
Ladle <77344665+The-Ladle@users.noreply.github.com>
2026-02-20 15:26:06 -0500

    More WIP

--- Assets/FuelManager.cs
+++ Assets/FuelManager.cs
@@ -1,52 +1,73 @@
 using UnityEngine;
+using static UnityEngine.InputSystem.HID.HID;
 
 public class FuelManager : MonoBehaviour
 {
-    public Rigidbody2D rb;
-
     public float dragCoefficient = 0.47f;
     public float airDensity = 1.225f;
 
-    float area;
+    public float g = 9.81f;
 
+    float area;
+    float mass = 0.226f;
+
+    public Vector2 v;
+    public Vector2 p;
     public Vector2 initV;
-    public Vector2 start;
+    public Vector2 initPos;
 
     public Vector2 end;
     public float maxHeight;
+    public bool simulating = false;
     public bool madeIt = false;
     public bool dead = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var launcher = GameObject.FindGameObjectsWithTag("Launcher")[0].GetComponent<Launcher>();
-        transform.position = launcher.getInitPos();
-        rb.linearVelocity = launcher.getInitVel();
-        initV = launcher.getInitV();
-        start = launcher.getInitStart();
         float r = transform.localScale.x * 0.5f;
         area = Mathf.PI * r * r;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void FixedUpdate()
     {
-        Vector2 v = rb.linearVelocity;
-
-        float speed = v.magnitude;
-
-        if (speed > 0f)
+        if (simulating)
         {
-            Vector2 dragForce =
-                -0.5f * airDensity * speed * speed * dragCoefficient * area * v.normalized;
+            this.p = transform.position;
+            if (p.y > maxHeight)
+            {
+                maxHeight = p.y;
+            }
 
-            rb.AddForce(dragForce);
+            Vector2 totalForces = new Vector2(0, -g * mass);
+
+            float speed = v.magnitude;
+
+            if (speed > 0f)
+            {
+                Vector2 dragForce =
+                    -0.5f * airDensity * speed * speed * dragCoefficient * area * v.normalized;
+
+                totalForces += dragForce;
+            }
+
+            Vector2 a = totalForces / mass;
+
+            this.v += a * Time.fixedDeltaTime;
+            transform.position += new Vector3(v.x * Time.fixedDeltaTime, v.y * Time.fixedDeltaTime);
         }
+    }
+
+    public void Launch(Vector2 pos, Vector2 vel)
+    {
+        this.p = pos;
+        this.initPos = pos;
+        transform.position = pos;
+        this.v = vel;
+        this.initV = vel;
+
+        maxHeight = p.y;
+
+        simulating = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -55,12 +76,12 @@ public class FuelManager : MonoBehaviour
         {
             madeIt = true;
         }
-        else if (collision.gameObject.CompareTag("Finish")
+        else if (collision.gameObject.CompareTag("Finish"))
         {
             dead = true;
             end = transform.position;
         }
-        else if (collision.gameObject.CompareTag("Kill")
+        else if (collision.gameObject.CompareTag("Kill"))
         {
             dead = true;
             madeIt = false;
--- Assets/Launcher.cs
+++ Assets/Launcher.cs
@@ -1,16 +1,38 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Launcher : MonoBehaviour
 {
     public bool hasStarted = false;
+    public Trajectory mostRecentTrajectory = new Trajectory { madeIt = false };
+    public Trajectory mostRecentSuccessfulTrajectory = new Trajectory { madeIt = true };
     public List<Trajectory> allTrajectories;
     public List<Trajectory> validTrajectories;
-    private Vector2 initPos;
-    private Vector2 initVel;
 
-    private int i;
-    private int j;
+    public Transform target;
+
+    public Transform child;
+    public GameObject fuel;
+
+    /*----ALL PARAMETERS FOR SHOTOER HERE----*/
+    public float minAngle;
+    public float maxAngle;
+
+    public float minSpeed;
+    public float maxSpeed;
+
+    public float minVX;
+    public float maxVX;
+
+    public float minX;
+    public float maxX;
+
+    public int angleRes;
+    public int vxRes;
+    public int xRes;
+    public int maxSpeedTries;
+
 
     void Start()
     {
@@ -19,12 +41,79 @@ public class Launcher : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !hasStarted)
-        {
-            hasStarted = true;
-        }
-        
+        //if (Input.GetKeyDown(KeyCode.Space) && !hasStarted)
+        //{
+        //    transform.eulerAngles = new Vector3(0,0,-45);
+        //    fuel.Launch(child.position, new Vector2(10.0f, 10.0f));
+        //}
+    }
 
+    IEnumerator Simulate(float robotX, float robotVX, float angleDegs, float launchSpeed)
+    {
+        GameObject obj = Instantiate(fuel, child.position, Quaternion.identity);
+        FuelManager manager = obj.GetComponent<FuelManager>();
+
+        Vector2 angleUnitVector = new Vector2(Mathf.Cos(angleDegs * Mathf.Deg2Rad), Mathf.Sin(angleDegs * Mathf.Deg2Rad));
+        Vector2 launchVector = angleUnitVector * launchSpeed;
+        manager.Launch(child.position, launchVector);
+
+        yield return new WaitUntil(() => manager.dead);
+
+        Trajectory traj = new Trajectory
+        {
+            initX = robotX,
+            initVX = robotVX,
+            initTheta = angleDegs,
+            initSpeed = launchSpeed,
+
+            madeIt = manager.madeIt,
+            maxHeight = manager.maxHeight,
+            landingX = manager.end.x,
+            landingY = manager.end.y,
+        };
+
+        mostRecentTrajectory = traj;
+        allTrajectories.Add(traj);
+
+        Destroy(obj);
+
+        yield return null;
+    }
+
+    IEnumerator BinarySearch(float robotX, float robotVX, float angleDegs)
+    {
+        float pivot = (maxSpeed + minSpeed) / 2;
+        float currentMaxSpeed = maxSpeed;
+        float currentMinSpeed = minSpeed;
+        int i = 0;
+        bool successful = false;
+
+        transform.eulerAngles = new Vector3(0, 0, -angleDegs);
+
+        while (!mostRecentTrajectory.madeIt && i < maxSpeedTries)
+        {
+            if (mostRecentTrajectory.landingX != null)
+            {
+                if (mostRecentTrajectory.landingX > target.position.x)
+                {
+                    currentMinSpeed = pivot;
+                } else
+                {
+                    currentMaxSpeed = pivot;
+                }
+            }
+            pivot = (maxSpeed + minSpeed) / 2;
+            yield return StartCoroutine(Simulate(robotX, robotVX, angleDegs, pivot));
+            i++;
+        }
+        if (!mostRecentTrajectory.madeIt)
+        {
+            mostRecentSuccessfulTrajectory = null;
+        } else
+        {
+            mostRecentSuccessfulTrajectory = mostRecentTrajectory;
+        }
+        yield return null;
     }
 
     public class Trajectory
@@ -36,15 +125,7 @@ public class Launcher : MonoBehaviour
 
         public bool madeIt;
         public float maxHeight;
-    }
-
-    public Vector2 getInitPos()
-    {
-        return initPos;
-    }
-
-    public Vector2 getInitVel()
-    {
-        return initVel;
+        public float landingX;
+        public float landingY;
     }
 }
